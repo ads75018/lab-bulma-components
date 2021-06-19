2ff986c90ff4ef27c042d2ec35629a903c620e8d
Adam Ghaoul <adamghaoul@gmail.com>
2021-06-19 12:51:34 +0200

    1st iteration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
+import NavBar from '../src/navbar/Navbar'
 
 const App = () => {
-  return <h1>Bulma</h1>;
-};
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+)};
 
 export default App;
